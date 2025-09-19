ff5beae66a9b90919cd38e02ff0f382a9e2292c5
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-20 00:01:56 +0200

    Change CSS import from index.css to styles.css
    Replaced the import of './index.css' with './styles.css'.

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./styles.css";
+import "./styles.css"; // ⬅️ remplace l’ancien import './index.css'
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
