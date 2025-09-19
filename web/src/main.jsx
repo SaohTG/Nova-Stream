1ef572f298db54a60f3a140aa7cf1811e9b73aec
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-20 01:57:56 +0200

    Rename style.css to styles.css

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./style.css";
+import "./styles.css";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
