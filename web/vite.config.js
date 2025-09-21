b3377db4e2d7434a8695603a8420ff6c8f7b7962
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-21 19:50:16 +0200

    Enhance Vite config with allowedHosts settings

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, host: true }
-})
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ["lector.lorna.tv"],
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    allowedHosts: ["lector.lorna.tv"],
+  },
+});
