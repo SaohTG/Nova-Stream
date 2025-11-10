4caaa6985c7ee461d450022a470a286ca7e19bff
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-11-10 04:05:46 +0100

    Update Layout.jsx

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -55,9 +55,9 @@ export default function Layout({ children }) {
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
                 </svg>
-                Nova
+                Lorna
               </span>{" "}
-              <span className="text-zinc-100 group-hover:text-white transition-colors">Stream</span>
+              <span className="text-zinc-100 group-hover:text-white transition-colors">TV</span>
             </Link>
 
             {/* Desktop nav */}
